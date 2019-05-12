5bd435819171bce999250bf5321e8dc70820b503
Paul Holmes <paulholmes1993@gmail.com>
2019-05-12 00:29:48 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,13 @@
 # Write a speak_to_grandma method.
 
+def speak_to_grandma(phrase)
+  phrase = "Hey Grandma!"
+
+
+
+
+
+
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
