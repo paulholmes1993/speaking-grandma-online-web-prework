30549db642a2ed18af50433ebeeaf400212328dd
Paul Holmes <paulholmes1993@gmail.com>
2019-05-12 00:36:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,14 @@
 # Write a speak_to_grandma method.
 
 def speak_to_grandma(phrase)
-  if phrase == 
+  if phrase == phrase.upcase
+    puts "NO, NOT SINCE 1938!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  else
+    puts "HUH?! SPEAK UP, SONNY!"
+end 
+  
 
 
 
